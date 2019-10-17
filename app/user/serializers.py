066405ef59f288007f01a1e513a36c64347feb98
Daniel Ionita <daniel@regentsitsolutions.co.uk>
2019-10-17 16:36:48 +0100

    added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,6 +15,15 @@ class UserSerializer(serializers.ModelSerializer):
         """ Create a new user with encrypted password and return it """
         return get_user_model().objects.create_user(**validate_data)
 
+    def update(self, instance, validated_data):
+        """ Update a new user with encrypred password and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if  password:
+            user.set_password(password)
+            user.save()
+        return user
+
 class AuthTokenSerializer(serializers.Serializer):
     """ Serializer for thje user auth object """
     email = serializers.CharField()
