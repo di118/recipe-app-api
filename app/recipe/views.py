d70ff95cb42e90e438f51d94e79eae96a5a743b6
Daniel Ionita <daniel@regentsitsolutions.co.uk>
2019-10-22 21:27:27 +0100

    Allow users to view detailed recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,8 +38,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, )
 
+
     def get_queryset(self):
         """ Retrieve the recipies for the authenticated user """
         return self.queryset.filter(user=self.request.user)
 
+    def get_serializer_class(self):
+        """ Return apropriate serializer class """
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+            
+        return self.serializer_class
 
